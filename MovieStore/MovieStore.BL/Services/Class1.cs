e6923379e96fbcbee1534d3f72212f0f6da3d5fe
KristiyanYordanov02 <kingroalbg@gmail.com>
2024-12-02 19:02:58 +0200

    update

--- MovieStore/MovieStore.BL/Services/Class1.cs
+++ MovieStore/MovieStore.BL/Services/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieStore.BL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace MovieStore.BL.Services
 {
-    internal class Class1
+    internal class MovieBLService
     {
+        private readonly IMovieBLService _movieBLService;
+        private readonly IActorRepository _actorRepository;
     }
+
+    public IEnumarable<>
 }
